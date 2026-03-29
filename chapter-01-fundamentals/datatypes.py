c1030e10a0b94000b439c0471a258719eb2c2359
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2026-03-29 19:58:32 +0530

    Python - Basics

---
+++ chapter-01-fundamentals/datatypes.py
@@ -0,0 +1,20 @@
+""" 
+2. Core Data Types
+
+
+"""
+# Numbers & Strings
+
+age = 25          # int
+price = 99.9      # float
+name = "Nithin"   # str
+is_active = True  # bool
+
+# List (Like C# List<T>)
+
+nums = [1, 2, 3, 4]
+mixed = [1, "hello", True]
+
+#Ordered (keeps the elements in the same sequence you add them),Mutable (you can modify the list after creating it)
+
+
---
+++ chapter-01-fundamentals/variables.py
@@ -0,0 +1,28 @@
+""" 
+Python is dynamically typed
+Type is decided at runtime
+
+"""
+age = 25
+name = "Nithin"
+
+x = 10
+x = "hello"  # totally valid
+
+""" 
+Python is strongly typed but dynamically typed ❌ No implicit conversion like JS ✅ But type is decided at runtime
+
+Strongly typed means every variable has a fixed type, and Python won’t mix incompatible types automatically.
+For example, "5" + 2 will throw an error instead of converting "5" to a number.
+This is different from JavaScript, which may implicitly convert types.
+
+Dynamically typed means you dont declare the type explicitly.
+The type is decided at runtime based on the value assigned.
+Example: x = 10 → x is int, later x = "hello" → now x is string.
+So Python enforces type safety (strong typing) but determines types automatically (dynamic typing).
+In short: flexible assignment, strict usage.
+
+
+"""
+"10" + 5  # ❌ Error
+
